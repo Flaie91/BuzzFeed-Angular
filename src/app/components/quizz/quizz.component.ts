d28e0da0f1319b26796756fa759d96576feb9ae4
Flaie <flaie9@gmail.com>
2023-12-02 10:04:56 -0300

    component creation

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+
+}
